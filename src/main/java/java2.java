dee6d51180a18618043f998bef101bc1aee9d66c
Ayushi <info@radii.in>
2020-01-17 14:57:56 +0530

    initial stage

---
+++ src/main/java/java2.java
@@ -0,0 +1,18 @@
+class current 
+{
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("currentThread:"+t);
+        t.setName("MyThread");
+        System.out.println("Thread2:"+t);
+        try{
+            for(int n=5;n>0;n--){
+                System.out.println(n);
+                Thread.sleep(1000);            }
+        }
+        catch(InterruptedException e){
+            System.out.println("main Thread Interupted");
+        
+    }
+}
+}
